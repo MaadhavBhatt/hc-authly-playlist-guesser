cf32ffddf464f623e87b77536263d9b38f572925
Maadhav Bhatt <81471860+MaadhavBhatt@users.noreply.github.com>
2025-08-07 20:17:16 +0530

    feat: implement LoginCard component and update App.vue

--- src/App.vue
+++ src/App.vue
@@ -1,15 +1,15 @@
 <template>
   <div id="app">
-    <div class="login">
-      <h1>Log In</h1>
-      <p>Some cool subtitle</p>
-    </div>
+    <LoginCard />
   </div>
 </template>
 
 <script>
+import LoginCard from './components/LoginCard.vue'
+
 export default {
   name: 'App',
+  components: { LoginCard },
 }
 </script>
 
@@ -30,24 +30,5 @@ export default {
   display: grid;
   place-items: center;
   background-color: #8c91a5;
-}
-
-.login {
-  width: min(80%, 600px);
-  margin: 0 auto;
-  padding: 2rem;
-  background-color: #333;
-  box-shadow: 1rem 1rem 0 rgba(255, 255, 255, 0.7);
-
-  & h1 {
-    font-size: 3rem;
-    font-weight: 700;
-    color: #f0f0f0;
-  }
-
-  & p {
-    font-size: 1.6rem;
-    color: #eee;
-  }
 }
 </style>
